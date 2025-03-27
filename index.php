c37c9844f6b00391ed9b3ad7af825757d38d7878
axoblade <axoblade@gmail.com>
2025-03-27 16:50:28 +0300

    update

--- index.php
+++ index.php
@@ -205,75 +205,7 @@
                             </ul>
                         </li>
                         <li class="submenu">
-                            <!--<a href="javascript:void(0);"><img src="assets/img/icons/places.svg" alt="img"><span> Places</span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="newcountry.html">New Country</a></li>
-<li><a href="countrieslist.html">Countries list</a></li>
-<li><a href="newstate.html">New State </a></li>
-<li><a href="statelist.html">State list</a></li>
-</ul>
-</li>
-<li>
-<a href="components.html"><i data-feather="layers"></i><span> Components</span> </a>
-</li>
-<li>
-<a href="blankpage.html"><i data-feather="file"></i><span> Blank Page</span> </a>
-</li>
-<li class="submenu">
-<a href="javascript:void(0);"><i data-feather="alert-octagon"></i> <span> Error Pages </span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="error-404.html">404 Error </a></li>
-<li><a href="error-500.html">500 Error </a></li>
-</ul>
-</li>
-<li class="submenu">
-<a href="javascript:void(0);"><i data-feather="box"></i> <span>Elements </span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="sweetalerts.html">Sweet Alerts</a></li>
-<li><a href="tooltip.html">Tooltip</a></li>
-<li><a href="popover.html">Popover</a></li>
-<li><a href="ribbon.html">Ribbon</a></li>
-<li><a href="clipboard.html">Clipboard</a></li>
-<li><a href="drag-drop.html">Drag & Drop</a></li>
-<li><a href="rangeslider.html">Range Slider</a></li>
-<li><a href="rating.html">Rating</a></li>
-<li><a href="toastr.html">Toastr</a></li>
-<li><a href="text-editor.html">Text Editor</a></li>
-<li><a href="counter.html">Counter</a></li>
-<li><a href="scrollbar.html">Scrollbar</a></li>
-<li><a href="spinner.html">Spinner</a></li>
-<li><a href="notification.html">Notification</a></li>
-<li><a href="lightbox.html">Lightbox</a></li>
-<li><a href="stickynote.html">Sticky Note</a></li>
-<li><a href="timeline.html">Timeline</a></li>
-<li><a href="form-wizard.html">Form Wizard</a></li>
-</ul>
-</li>
-<li class="submenu">
-<a href="javascript:void(0);"><i data-feather="bar-chart-2"></i> <span> Charts </span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="chart-apex.html">Apex Charts</a></li>
-<li><a href="chart-js.html">Chart Js</a></li>
-<li><a href="chart-morris.html">Morris Charts</a></li>
-<li><a href="chart-flot.html">Flot Charts</a></li>
-<li><a href="chart-peity.html">Peity Charts</a></li>
-</ul>
-</li>
-<li class="submenu">
-<a href="javascript:void(0);"><i data-feather="award"></i><span> Icons </span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="icon-fontawesome.html">Fontawesome Icons</a></li>
-<li><a href="icon-feather.html">Feather Icons</a></li>
-<li><a href="icon-ionic.html">Ionic Icons</a></li>
-<li><a href="icon-material.html">Material Icons</a></li>
-<li><a href="icon-pe7.html">Pe7 Icons</a></li>
-<li><a href="icon-simpleline.html">Simpleline Icons</a></li>
-<li><a href="icon-themify.html">Themify Icons</a></li>
-<li><a href="icon-weather.html">Weather Icons</a></li>
-<li><a href="icon-typicon.html">Typicon Icons</a></li>
-<li><a href="icon-flag.html">Flag Icons</a></li>
-</ul>
-</li>-->
+
                         <li class="submenu">
                             <a href="javascript:void(0);"><i data-feather="columns"></i> <span> Forms </span> <span
                                     class="menu-arrow"></span></a>
