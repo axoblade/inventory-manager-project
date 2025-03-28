84b95e345a3d6589c2cb6e4cfd3b473153c7dff8
Mpirirwe Tabitha <mpirirwetabitha0@gmail.com>
2025-03-28 16:31:27 +0300

    made changes on the navigation bar

--- navbar.php
+++ navbar.php
@@ -12,13 +12,8 @@
                     <ul>
                         <li><a href="productlist.php">Product List</a></li>
                         <li><a href="addproduct.html">Add Product</a></li>
-                        <li><a href="categorylist.html">Category List</a></li>
-                        <li><a href="addcategory.html">Add Category</a></li>
-                        <li><a href="subcategorylist.html">Sub Category List</a></li>
-                        <li><a href="subaddcategory.html">Add Sub Category</a></li>
-                        <li><a href="brandlist.html">Brand List</a></li>
-                        <li><a href="addbrand.html">Add Brand</a></li>
-                        <li><a href="importproduct.html">Import Products</a></li>
+                        
+                    
                     </ul>
                 </li>
                 <li class="submenu">
