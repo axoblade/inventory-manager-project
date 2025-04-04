e119788e002a4b0e697958e444cbe6889cf7d662
Mpirirwe Tabitha <mpirirwetabitha0@gmail.com>
2025-04-04 13:02:16 +0300

    DB changes

--- db.php
+++ db.php
@@ -14,10 +14,10 @@ class MySQLDatabase
     {
         $host = "localhost";
         $username = "root";
-        $password = "root";
+        $password = "";
         $database = "inventory";
-        $port = 8889;
-        $this->connection = new mysqli($host, $username, $password, $database, $port);
+        /*$port = 8889;*/
+        $this->connection = new mysqli($host, $username, $password, $database);
 
         if ($this->connection->connect_error) {
             die("Connection failed: " . $this->connection->connect_error);
